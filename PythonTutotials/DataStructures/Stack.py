07f81583747f6e736b655d1600bfeb70b300f6cd
Amit Majumdar <amitmajumdar@Amits-iMac.local>
2017-11-14 18:30:39 +0530

    Stack And Queue

---
+++ PythonTutotials/DataStructures/Stack.py
@@ -0,0 +1,30 @@
+
+
+"""
+
+The list methods make it very easy to use a list as a stack,
+where the last element added is the first element retrieved (“last-in, first-out”).
+To add an item to the top of the stack, use append().
+To retrieve an item from the top of the stack, use pop() without an explicit index. For example:
+
+"""
+
+stack = [3, 4, 5]
+print(stack)
+
+stack.append(6)
+stack.append(7)
+
+print(stack)
+
+
+stack.append(8)
+
+print(stack)
+
+
+stack.pop()
+print(stack)
+
+stack.pop(3)
+print(stack)
